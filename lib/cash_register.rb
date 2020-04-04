d9d2a0653097aece9352bab9f08fd7530e254e0d
CiaraMaria Proia <proiacm@gmail.com>
2020-04-04 19:32:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ end
 
 def apply_discount
   @total -= (@total * @discount.to_f / 100)
-  "After discount, the total comes to #{total.to_i}."
+  "After discount, the total comes to $#{total.to_i}."
 
 end 
 
